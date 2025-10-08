a7318ebe53465a9870f6e315a2389f001a4f8369
unknown <gui_o_brito@yahoo.com.br>
2025-10-08 15:16:05 -0300

    refactor: refatorada classe ITAE

--- core/itae.py
+++ core/itae.py
@@ -1,12 +1,19 @@
 class ITAE:
+    A = 0.965
+    B = -0.85
+    C = 0.796
+    D = -0.147
+    E = 0.308
+    F = 0.929
+
     @staticmethod
     def kp(k, theta, tau):
-        return ((0.965/k)*((theta/tau)**(-0.85)))
+        return ((ITAE.A/k)*((theta/tau)**(ITAE.B)))
     
     @staticmethod
-    def ti(k, theta, tau):
-        return ((tau)/(0.796-0.147*(theta/tau)))
+    def ti(theta, tau):
+        return ((tau)/((ITAE.C+ITAE.D)*(theta/tau)))
     
     @staticmethod
     def td(theta, tau):
-        return (tau*0.308*(theta/tau)**0.929)
+        return (tau*ITAE.E*(theta/tau)**ITAE.F)
